d28255b95b319f4b8a7866d9b00536a40a935d98
mendoitarou <42207304+mendoitarou@users.noreply.github.com>
2026-04-07 00:04:47 +0900

    Change_CheckUseFunction,Add_CheckRoleId

--- discord_tool_bot/events/Chat_Reading.js
+++ discord_tool_bot/events/Chat_Reading.js
@@ -26,35 +26,73 @@ module.exports = {
             } else {
                 // 読み上げテキストを作成
                 let regex_UserId = /<@[0-9]{17,19}>/g;// ユーザIdの正規表現
+                let regex_RoleId = /<@&[0-9]{19}>/g;// ロールIdの正規表現
                 let regex_emoji = /(?:^|[^<]):([^:\s]+):|<:([^:\s]+):(\d+)>/g;// 絵文字の正規表現
                 let regex_URL = /https?:\/\/(?:[-\w.])+(?::[0-9]+)?(?:\/(?:[\w\/_.])*(?:\?(?:[\w&=%.])*)?(?:#(?:[\w.])*)?)?/g;// URL検出用の正規表現
                 const message = `${interaction.content}`;// 受信したメッセージをいったん格納
+
+                function checkURL(receiveMessage) { return (receiveMessage.match(regex_URL) !== null) }
+                function checkUserId(receiveMessage) { return (receiveMessage.match(regex_UserId) !== null ) }
+                async function replaceUserId(receiveMessage) {
+                    // ユーザID置き換え
+                    if(checkUserId(receiveMessage)) {
+                        await receiveMessage.match(regex_UserId).forEach(async get_UserId => {// forEachは非同期処理？みたいなのでawaitする
+                            // メンション箇所ごとに実行
+                            let User_Id = get_UserId.replace("<@", "").replace(">", "");// ユーザIDのみに加工
+                            let get_member = await interaction.guild.members.fetch(`${User_Id}`);// ユーザIDからサーバ内のmemberを取得
+                            receiveMessage = receiveMessage.replace(`${get_UserId}`, `、${get_member.displayName}`);// ユーザIDの箇所を表示名に置き換え
+                        });
+                        return receiveMessage;
+                    } else {
+                        return receiveMessage;// 検知しなかったらそのまま返す
+                    }
+                }
+
+                function checkRoleId(receiveMessage) { return (receiveMessage.match(regex_RoleId) !== null ) }
+                async function replaceRoleId(receiveMessage) {
+                    // ロールID置き換え
+                    if(checkRoleId(receiveMessage)) {
+                        // ロール検知
+                        await receiveMessage.match(regex_RoleId).forEach(async get_RoleId => {// forEachは非同期処理？みたいなのでawaitする
+                            let Role_Id = get_RoleId.replace("<@&", "").replace(">", "");// ロールIDのみに加工
+                            let get_role = await interaction.guild.roles.fetch(`${Role_Id}`);// ロールIDからサーバ内のrolesを取得
+                            receiveMessage = receiveMessage.replace(`${get_RoleId}`, `${get_role.name}`);// ロールIDの箇所をロール名に置き換え
+                        });
+                        return receiveMessage;
+                    } else {
+                        return receiveMessage;// 検知しなかったらそのまま返す
+                    }
+                }
+
+                function checkUserEmoji(receiveMessage) { return (receiveMessage.match(regex_emoji) !== null ) }
+                function replaceUserEmoji(receiveMessage) {
+                    // 絵文字置き換え
+                    if(checkUserEmoji(receiveMessage)) {
+                        // 絵文字検知
+                        if(receiveMessage.match(regex_emoji) !== null) {
+                            return receiveMessage.replace(regex_emoji, '絵文字');// 置き換えたものを返す
+                        }
+                    } else {
+                        return receiveMessage;// 検知しなかったらそのまま返す
+                    }
+                }
                 
                 // URLが含まれているかチェック
-                if(message.match(regex_URL) !== null) {
+                if(checkURL(message)) {
                     text = `${member.displayName}さんがURLを送信しました。`;
-                } else if(message.match(regex_UserId) !== null) {
-                    // メンションしてたら、ユーザIDをユーザ名に置き換え
+                } else {
+                    // 普通にメッセージだったら
                     let receive_Message = message;
-                    await message.match(regex_UserId).forEach(async get_UserId => {// forEachは非同期処理？みたいなのでawaitする
-                        // メンション箇所ごとに実行
-                        let User_Id = get_UserId.replace("<@", "").replace(">", "");// ユーザIDのみに加工
-                        let get_member = await interaction.guild.members.fetch(`${User_Id}`);// ユーザIDからサーバ内のmemberを取得
-                        receive_Message = receive_Message.replace(`${get_UserId}`, `、${get_member.displayName}`);// ユーザIDの箇所を表示名に置き換え
-                    });
-                    // 絵文字検知
-                    if(message.match(regex_emoji) !== null) {
-                        receive_Message = receive_Message.replace(regex_emoji, '絵文字');
+                    if(checkUserId(receive_Message)) {
+                        receive_Message = await replaceUserId(receive_Message);// ユーザIDチェック
+                    }
+                    if(checkRoleId(receive_Message)) {
+                        receive_Message = await replaceRoleId(receive_Message);// ロールIDチェック
+                    }
+                    if(checkUserEmoji(receive_Message)) {
+                        receive_Message = replaceUserEmoji(receive_Message);// ユーザー絵文字チェック
                     }
                     text = `${member.displayName}さんのメッセージ、${receive_Message.replace(/r?n/g, '、')}`;// 最終的なメッセージを指定
-                } else {
-                    // メンションしてないならそのまま
-                    // 絵文字検知
-                    let receive_Message = message;
-                    if(message.match(regex_emoji) !== null) {
-                        receive_Message = receive_Message.replace(regex_emoji, '絵文字');
-                    }
-                    text = `${member.displayName}さんのメッセージ、${receive_Message.replace(/r?n/g, '、')}`;// 特に制限などは設けていない。必要そうであれば処理をここの上に追加。
                 }
                 console.log(`Text: ${text}`);
             }
