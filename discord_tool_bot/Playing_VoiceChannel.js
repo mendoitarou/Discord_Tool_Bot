b2f3cbdf12753903f98cd1e5c7991a0a24242b13
mendoitarou <42207304+mendoitarou@users.noreply.github.com>
2026-07-04 23:20:20 +0900

    Add_Read-Queue

--- discord_tool_bot/Playing_VoiceChannel.js
+++ discord_tool_bot/Playing_VoiceChannel.js
@@ -1,22 +1,35 @@
+const fs = require('fs');
+
 const { NoSubscriberBehavior, createAudioPlayer, createAudioResource, AudioPlayerStatus } = require('@discordjs/voice');
 
-function play_resource(voicechannel_connection, source='./output.wav') {
+function play_resource(voicechannel_connection, source = './output.wav') {
     //console.log("[Playing_VoiceChannel.js]Do_play-resource");
-    const resource = createAudioResource(source);
+    return new Promise((resolve, reject) => {
+        const resource = createAudioResource(source);
+        // ボイスチャットでの再生処理
+        const player = createAudioPlayer({
+            behaviors: {
+                noSubscriber: NoSubscriberBehavior.Pause,
+            },
+        });
+        // 再生開始
+        player.play(resource);
+        const subscriber = voicechannel_connection.subscribe(player);
+        
+        // 待機
+        player.on(AudioPlayerStatus.Idle, () => {
+            // 再生が終わったので解除
+            subscriber.unsubscribe();
+            fs.unlinkSync(source); // ファイル削除
+            resolve();
+        });
 
-    // ボイスチャットでの再生処理
-    player = createAudioPlayer({
-        behaviors: {
-            noSubscriber: NoSubscriberBehavior.Pause,
-        },
-    });
-    // 再生開始
-    player.play(resource);
-    subscriber = voicechannel_connection.subscribe(player);
-    player.on(AudioPlayerStatus.Idle, () => {
-        // 再生が終わったので解除
-        subscriber.unsubscribe();
-        return;
+        // エラー
+        player.on('error', (error) => {
+            subscriber.unsubscribe();
+            fs.unlinkSync(source); // ファイル削除
+            reject(error);
+        });
     });
 }
 
