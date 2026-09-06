85e8bdebd5edb30fca9c852f0aa8e11fc4471afb
mendoitarou <42207304+mendoitarou@users.noreply.github.com>
2026-09-06 22:22:12 +0900

    Fix_Miss

--- discord_tool_bot/events/interactionCreate.js
+++ discord_tool_bot/events/interactionCreate.js
@@ -2,7 +2,7 @@ const { Events, MessageFlags } = require('discord.js');
 
 module.exports = {
 	name: Events.InteractionCreate,
-	async execute(interaction) {
+	async execute(interaction, { services }) {
 		if (!interaction.isChatInputCommand()) return;
 
 		const command = interaction.client.commands.get(interaction.commandName);
@@ -13,7 +13,7 @@ module.exports = {
 		}
 
 		try {
-			await command.execute(interaction);
+			await command.execute(interaction, { services });
 		} catch (error) {
 			console.error(error);
 			if (interaction.replied || interaction.deferred) {
--- discord_tool_bot/index.js
+++ discord_tool_bot/index.js
@@ -40,7 +40,7 @@ for (const folder of commandFolders) {
 		const filePath = path.join(commandsPath, file);
 		const command = require(filePath);
 		if ('data' in command && 'execute' in command) {
-			client.commands.set(command.data.name, { command, services });
+			client.commands.set(command.data.name, command);
 		} else {
 			console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
 		}
